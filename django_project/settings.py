1c5639e01f58806248cda09cc95d80d1f814556f
Lucas Silva <lucassempel@hotmail.com>
2023-12-19 23:22:26 -0800

    ch6

--- django_project/settings.py
+++ django_project/settings.py
@@ -108,6 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = "accounts.CustomUser"
 
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 

ec09d58606a925aca8a4553370ce9abdb7d7a3b7
Lucas Silva <lucassempel@hotmail.com>
2023-12-20 23:20:17 -0800

    ch11

--- django_project/urls.py
+++ django_project/urls.py
@@ -3,9 +3,10 @@ from django.urls import path, include
 
 urlpatterns = [
     # Django admin
-    path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls, name="admin"),
     # User management
     path("accounts/", include("allauth.urls")),
     # Local apps
     path("", include("pages.urls")),
+    path("books/", include("books.urls")),
 ]
